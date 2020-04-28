ac738e2c99db190ead663e5a1d6b7ffccaa737a3
juhishaw <juhishw@gmail.com>
2020-04-28 23:14:48 +0530

    Updates

---
+++ precache-manifest.b9563d78c16507a00f3002c91d8dd3b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064f1141e9f7bce2e838e3dc35c55073",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "96c9df3929b98fde57f9",
+    "url": "/monsters-rolodex/static/css/main.a575bb4c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c9df3929b98fde57f9",
+    "url": "/monsters-rolodex/static/js/main.d7343f1f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
